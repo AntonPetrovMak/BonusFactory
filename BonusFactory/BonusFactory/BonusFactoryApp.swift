8be3c724d65265d115d403c76c23db2a41ac9577
antonpetrovmak <antonpetrovmak@gmail.com>
2021-11-25 01:04:14 +0200

    init project

---
+++ BonusFactory/BonusFactory/BonusFactoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonusFactoryApp.swift
+//  BonusFactory
+//
+//  Created by Petrov Anton on 25.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct BonusFactoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
